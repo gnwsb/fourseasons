661edf9c73961e90a9c1c9e43383a7e882ec4c64
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-07-03 13:34:55 +0900

    rollback

---
+++ app/src/main/java/com/example/seasonsapp/SpringActivity.kt
@@ -0,0 +1,11 @@
+package com.example.seasonsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SpringActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spring)
+    }
+}
